6fb9043a957a46fb6d8a2f2754722b195831ed52
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-01-08 15:10:33 +0200

    WIP: Alternative BOM Lines - Data Entry Setup
    * Item Alternatives
    * Item Alternative

--- erpnext/stock/doctype/item/item_dashboard.py
+++ erpnext/stock/doctype/item/item_dashboard.py
@@ -10,7 +10,8 @@ def get_data():
 			'Production Order': 'production_item',
 			'Product Bundle': 'new_item_code',
 			'BOM': 'item',
-			'Batch': 'item'
+			'Batch': 'item',
+			'Item Alternatives': 'item',
 		},
 		'transactions': [
 			{
@@ -40,7 +41,7 @@ def get_data():
 			},
 			{
 				'label': _('Manufacture'),
-				'items': ['Production Order']
+				'items': ['Production Order', 'Item Alternatives']
 			}
 		]
 	}
